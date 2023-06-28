841e83bca615b1c29ab5c3a3f551e158be249a00
SteveFranck <steve.nouta27@gmail.com>
2023-06-27 20:00:00 -0700

    first commit

---
+++ 0x05-pointers_arrays_string/0-reset_to_98.c
@@ -0,0 +1,8 @@
+/**
+ * Reset o 98- updates the value of the paremeter to 98
+ * integer pointer to a variable to be updated
+ * returning:nothinig
+ */
+void reset_to_98(int *n)
+	*n=98;
+
---
+++ 0x05-pointers_arrays_string/1-swap.c
@@ -0,0 +1,7 @@
+/**
+ * swap-function that swap the value of two numbers
+ * these numbers are a and b
+ * return:nothing
+ */
+
+void swap_int(int *a, int *b)
